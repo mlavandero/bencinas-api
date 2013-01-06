fc7e944f034e4021133c33962c6fd4d5359dabc2
Mario Lavandero <lavandero@gmail.com>
2013-01-06 16:55:28 -0300

    Initial commit. Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BencinasApi::Application.config.secret_token = '7a2c1a4fc521057ed2cbd7f42a3a6d5987175dccfaec49d3c0a3abf5464ca51176d83113c6d2f7fbc49fe4334e9cf244e65f4d35ba5d9c4c809c1a1c066250af'
