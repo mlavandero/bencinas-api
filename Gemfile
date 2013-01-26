017bca8d8881ec9d6bc447203ac7943db1fb3041
Mario Lavandero <lavandero@gmail.com>
2013-01-26 00:17:38 -0300

    Agregada gema headless, por problemas para partir X Server en ubuntu.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ end
 group :production do
   gem "execjs"
   gem "therubyracer", :require => 'v8'
+  gem 'headless'
 end
 
 gem 'jquery-rails'
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'headless'
 
 # Initialize the rails application
 BencinasApi::Application.initialize!
@@ -7,3 +8,9 @@ BencinasApi::Application.initialize!
 Capybara.run_server = false
 Capybara.current_driver = :webkit
 Capybara.app_host = "http://www.bencinaenlinea.cl/web2/buscador.php?region=7"
+
+# don't run on the local machine (since we don't have xvfb running locally)
+if Rails.env.production?
+    headless = Headless.new
+    headless.start
+end
