c67cce4d38f04ab4e0b6798bd4543e54306d6e8b
Mario Lavandero <lavandero@gmail.com>
2013-01-25 22:40:05 -0300

    #1 Implementacion scrapping
    Se utiliza Capybara (gema para test funcionales) para realizar el 
    scrapping.  Se debe configurar en environment.rb los datos para 
    Capybara.  Se guarda en el modelo los precios minimos, maximos y promedio.

--- app/models/informacion_bencina.rb
+++ app/models/informacion_bencina.rb
@@ -1,7 +1,15 @@
 class InformacionBencina < ActiveRecord::Base
   attr_accessible :comuna, :precio_maximo, :precio_minimo, :precio_promedio, :region
 
+  include Capybara::DSL
+
   def actualizar_precios
-    
+    visit('')
+    select 'Gasolina 95', :from => 'reporte_combustible'
+    click_button "Ver reporte"
+    self.precio_minimo = find('table#tabla_resumen td:nth-child(1) b').text[/[0-9\.]+/]
+    self.precio_maximo = find('table#tabla_resumen td:nth-child(2) b').text[/[0-9\.]+/]
+    self.precio_promedio = find('table#tabla_resumen td:nth-child(3) b').text[/[0-9\.]+/]
+    self.save
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BencinasApi::Application.initialize!
+
+Capybara.run_server = false
+Capybara.current_driver = :webkit
+Capybara.app_host = "http://www.bencinaenlinea.cl/web2/buscador.php?region=7"
