b0a3a608036d9b4a751ad3ef7c47c5db4924391d
Mario Lavandero <mario@mlavandero.org>
2013-02-06 22:12:16 -0300

    Probando subiendo el tiempo de espera carga de resultados del formulario.

--- app/models/informacion_bencina.rb
+++ app/models/informacion_bencina.rb
@@ -9,7 +9,7 @@ class InformacionBencina < ActiveRecord::Base
     visit('')
     select self.combustible.nombre, :from => 'reporte_combustible'
     click_button "Ver reporte"
-    sleep 6
+    sleep 20
     self.precio_minimo = find('table#tabla_resumen td:nth-child(1) b').text[/[0-9\.]+/]
     self.precio_maximo = find('table#tabla_resumen td:nth-child(2) b').text[/[0-9\.]+/]
     self.precio_promedio = find('table#tabla_resumen td:nth-child(3) b').text[/[0-9\.]+/]
