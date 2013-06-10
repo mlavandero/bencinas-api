5a7274ebf6d02250af9de9df3ee00bc35448560b
Mario Lavandero <mario@mlavandero.org>
2013-06-09 22:23:31 -0400

    Cambio a Postgres

--- db/migrate/20130402004217_cambiar_relacion_comuna_en_informacion_bencina.rb
+++ db/migrate/20130402004217_cambiar_relacion_comuna_en_informacion_bencina.rb
@@ -1,7 +1,9 @@
 class CambiarRelacionComunaEnInformacionBencina < ActiveRecord::Migration
   def up
     rename_column :informaciones_bencina, :comuna, :comuna_id
-    change_column :informaciones_bencina, :comuna_id, :integer
+    #change_column :informaciones_bencina, :comuna_id, :integer
+    execute "alter table informaciones_bencina
+        alter column comuna_id type integer using cast(CASE comuna_id WHEN '' THEN NULL ELSE comuna_id END AS integer)"
     execute "UPDATE informaciones_bencina SET comuna_id=1"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503032239) do
+ActiveRecord::Schema.define(:version => 20130505023217) do
 
   create_table "combustibles", :force => true do |t|
     t.string   "nombre"
@@ -23,17 +23,18 @@ ActiveRecord::Schema.define(:version => 20130503032239) do
     t.string   "nombre"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "id_externo"
   end
 
   create_table "precios", :force => true do |t|
     t.string   "region"
     t.integer  "comuna_id"
-    t.integer  "combustible_id"
     t.decimal  "precio_minimo",   :precision => 10, :scale => 2
     t.decimal  "precio_maximo",   :precision => 10, :scale => 2
     t.decimal  "precio_promedio", :precision => 10, :scale => 2
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
+    t.integer  "combustible_id"
   end
 
   add_index "precios", ["combustible_id"], :name => "index_informaciones_bencina_on_combustible_id"
